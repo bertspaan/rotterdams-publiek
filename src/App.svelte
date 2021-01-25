d9073f4ce79345c5c82475fb0c92a5b204350d76
Bert Spaan <bert.spaan@gmail.com>
2021-01-25 12:17:05 +0100

    added leaflet map layers

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,68 @@
 <script>
 	import { onMount } from 'svelte'
 
+	// TODO: add year to each map
+	// TODO: generate GeoJSON boundary for each map, using annotation
 	import maps from '../maps.yaml'
 
-	let geojson
+	let locations
+	let locationsLayer
 
-	onMount(async () => {
-		const  map = L.map('map').setView([51.9200, 4.4895], 13)
-
-		const tileUrl = 'https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png'
-		L.tileLayer(tileUrl, {
-			maxZoom: 18
-		}).addTo(map)
-
-		// TODO add layercontrol
-		// make layer for each map in yaml
-
-		// L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-	})
-
-	async function fetchGeoJSON (url) {
-		const response = await fetch(url)
-		geojson = await response.json()
-
-		// TODO add buildings to map, only show correct buildings!
-		console.log(geojson)
-		// https://leafletjs.com/examples/geojson/
+	const emptyGeoJSON = {
+		type: 'FeatureCollection',
+		features: []
 	}
 
-	fetchGeoJSON('https://raw.githubusercontent.com/mmmenno/rotterdams-publiek/master/plekken/kaart/locaties.geojson')
+	function updateLocations () {
+		// TODO: only show correct buildings, based on map publication date!
+		locationsLayer.clearLayers()
+		locationsLayer.addData(locations)
+	}
+
+	onMount(async () => {
+		const leafletMap = L.map('map').setView([51.9200, 4.4895], 13)
+
+		const tileUrl = 'https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png'
+
+		const baseLayer = L.tileLayer(tileUrl, {
+			maxZoom: 18
+		}).addTo(leafletMap)
+
+		let layers = {}
+
+		maps
+			.filter((map) => map.tiles)
+			.forEach((map, index) => {
+				const layer = L.tileLayer(map.tiles, {
+					maxZoom: 18,
+					tms: true
+				})
+
+				layers[map.title] = layer
+
+				if (index === 0) {
+					layer.addTo(leafletMap)
+				}
+			})
+
+		L.control.layers({'Base map': baseLayer}, layers).addTo(leafletMap)
+
+		locationsLayer = L.geoJSON(emptyGeoJSON, {
+			style: {
+    		color: '#afe800',
+    		weight: 5,
+    		opacity: 0.65
+			}
+		}).addTo(leafletMap)
+	})
+
+	async function fetchLocations (url) {
+		const response = await fetch(url)
+		locations = await response.json()
+		updateLocations()
+	}
+
+	fetchLocations('https://raw.githubusercontent.com/mmmenno/rotterdams-publiek/master/plekken/kaart/locaties.geojson')
 </script>
 
 <main>
