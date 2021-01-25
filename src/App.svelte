4dfbb2c0824c539313d75e62ca26f3c2719d93ba
Bert Spaan <bert.spaan@gmail.com>
2021-01-25 11:44:34 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+	import { onMount } from 'svelte'
+
+	import maps from '../maps.yaml'
+
+	let geojson
+
+	onMount(async () => {
+		const  map = L.map('map').setView([51.9200, 4.4895], 13)
+
+		const tileUrl = 'https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png'
+		L.tileLayer(tileUrl, {
+			maxZoom: 18
+		}).addTo(map)
+
+		// TODO add layercontrol
+	})
+
+	async function fetchGeoJSON (url) {
+		const response = await fetch(url)
+		geojson = await response.json()
+
+		// TODO add buildings to map, only show correct buildings!
+		console.log(geojson)
+	}
+
+	fetchGeoJSON('https://raw.githubusercontent.com/mmmenno/rotterdams-publiek/master/plekken/kaart/locaties.geojson')
+</script>
+
+<main>
+	<div id="map"></div>
+</main>
+
+<style>
+	main {
+		width: 100%;
+		height: 100%;
+	}
+
+	#map {
+		width: 100%;
+		height: 100%;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
